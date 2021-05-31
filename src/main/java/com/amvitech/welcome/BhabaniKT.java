7848c7cf0ea1dee0d09bb8c9db5beb69dd7f917b
bhabsaho29 <bhabani002@gmail.com>
2021-06-01 00:20:00 +0530

    added sonar plugin in pom.xml

---
+++ src/main/java/com/amvitech/welcome/BhabaniKT.java
@@ -0,0 +1,12 @@
+public class BhabaniKT  
+{  
+public static void main(String args[])  
+{  
+//character array  
+char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'b', 'h', 'a', 'b', 'a', 'n', 'i', '-', 'k', 't'};  
+//constructor of the String class that parses char array as a parameter  
+String string = new String(ch);  
+//prints the string  
+System.out.println(string);  
+}  
+} 
